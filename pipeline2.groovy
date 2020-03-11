ae393993eb70a8cd1b47c086416d03d1b8914b89
Andrey Krushelnitskiy <andrey.krushelnitsky@myoffice.team>
2020-03-11 09:57:07 +0300

    edit pipeline

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,3 +1,22 @@
+def nexusPublisher2(Map args=[:]) {
+  def filename = args.get('file')
+  def repo = args.get('repo')
+  def credentialsId = args.get('credentialsId', 'ucs-registry-rdojenkins')
+
+  withCredentials([usernameColonPassword(credentialsId: credentialsId, variable: 'NEXUS_CREDENTIALS')]) {
+    env.NEXUS_FILENAME = filename
+    env.NEXUS_REPO = repo
+
+    sh '''
+       set +x
+       ARTIFACT_URL=https://mdcnexus.stageoffice.ru/repository/${NEXUS_REPO}/${NEXUS_FILENAME}
+       curl -i --user ${NEXUS_CREDENTIALS} --upload-file ${NEXUS_FILENAME} ${ARTIFACT_URL}
+
+       echo "Successfully uploaded ${NEXUS_FILENAME} to ${ARTIFACT_URL}"
+    '''
+  }
+}
+
 node('docker') {
     def artifact_name = "${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}.log"
     phase('list files in work directory') {
@@ -7,7 +26,7 @@ node('docker') {
         sh "echo ${BUILD_NUMBER} > ${artifact_name}"
     }
     phase('push to nexus') {
-        nexusPublisher file: artifact_name, repo: 'testrail-releases'
+        nexusPublisher2 file: artifact_name, repo: 'testrail-releases'
     }
     phase('list files in work directory') {
         sh "ls -la"
