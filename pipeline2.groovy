825012f7b4c63b25b0e143b7aba4d4386ed60c72
Andrey Krushelnitskiy <andrey.krushelnitsky@myoffice.team>
2020-03-11 09:52:31 +0300

    edit pipeline

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,14 +1,14 @@
 node('docker') {
+    def artifact_name = "${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}.log"
     phase('list files in work directory') {
         sh "ls -la"
     }
     phase('Create file') {
         sh '''
-        echo ${BUILD_NUMBER} > ${BUILD_NUMBER}.log
+        echo ${BUILD_NUMBER} > ${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}.log
         '''
     }
     phase('push to nexus') {
-        def artifact_name = "${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}.log"
         nexusPublisher file: artifact_name, repo: 'testrail-releases'
     }
     phase('list files in work directory') {
